290e4245cf45c3229d08a01077cacd954783f793
SixtusTheSixth <crossbiblewine@gmail.com>
2024-04-07 23:17:45 -0400

    button pop and cleanup code

--- myscript.js
+++ myscript.js
@@ -1,25 +1,7 @@
-/*
-
-function snailme() {
-	let snailEmoji = "🐌";
-	var snailElem = $("#snail");
-	snailElem.outerHTML = '<p style="font-size:20px" class="position-absolute start-0 top-50">' + snailEmoji + '</p>';
-	function move() {
-		snailElem.animate({left:'+=1px'});
-		window.requestAnimationFrame(move);
-		console.log('moving');
-	}
-	window.requestAnimationFrame(move);
-}
-$("#snail").click(snailme);
-
-*/
-
 function snailme() {
 	let snailEmoji = "🐌";
 	document.getElementById("snail").outerHTML 
 	= '<p id="snail" style="font-size:20px" class="position-relative">' + snailEmoji + '</p>';
-	// window.screen.availWidth and window.screen.availHeight
 	let snailElem = document.getElementById("snail");
 	let positionX = snailElem.offsetLeft;
 	let speedX = 3;
@@ -27,10 +9,6 @@ function snailme() {
 	const minXPosition = 0;
 	console.log(maxXPosition);
 	console.log(positionX);
-	// snailElem.style['margin'] = '0px 150px 15px 200px';
-	// snailElem.style.border = '1px solid red';
-	// console.log(snailElem.style);
-	// console.log(snailElem.innerHTML);
 	let stepCount = 0;
 	function step() {
 		positionX = positionX + speedX;
@@ -38,14 +16,21 @@ function snailme() {
 		if (positionX > maxXPosition || positionX < minXPosition) {
 			speedX = speedX * (-1);
 		}
-		// snailElem.style.transform = `translateX(${ positionX }px)`; // positionX + 'px';
-		// snailElem.setProperty("x", positionX, "important");
 		snailElem.style['margin-left'] = positionX + 'px';
-		// if (stepCount < 3) {window.requestAnimationFrame(step);}
 		window.requestAnimationFrame(step);
-		// console.log("position: " + positionX + " step count: "+ stepCount + "actual pos: " + snailElem.style['margin-left']);
 	}
 	window.requestAnimationFrame(step);
 }
 
 document.getElementById("snail").addEventListener('click', snailme);
+
+// for clickable dead link
+// some credit to chatGPT for this one lol
+
+const clickable = document.getElementsByClassName('clickable').item(0);
+clickable.addEventListener('click', function() {
+	clickable.classList.toggle('clicked');
+	setTimeout(function() {
+		clickable.classList.toggle('clicked');
+	}, 300);
+})
